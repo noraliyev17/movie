7df62ec6688ca9c5cff8bd03f4313cf7be9f6f5b
noral1yev17 <asliddinnoraliyev@gmail.com>
2022-11-17 14:45:07 +0500

    movie

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-"use strict"
+"use strict";
 
-movies.splice(50);
+movies.splice(100);
 
 //  console.log(movies);
-
-
-
 //    {
 //     "title": "Patton Oswalt: Annihilation",
 //     "year": 2017,
@@ -22,68 +19,157 @@ movies.splice(50);
 //     "bigThumbnail": "http://i3.ytimg.com/vi/4hZi5QaMBFc/maxresdefault.jpg"
 //   }
 
- const AllMovies=movies.map((movies)=>{
-    return{
-        title:movies.title,
-        year:movies.year,
-        time:`${Math.floor(movies.runtime/60) }h ${movies.runtime%60}m`,
-        lang:movies.language,
-        category:movies.categories,
-        summary:movies.summary,
-        rating:movies.imdbRating,
-        ID:movies.imdbId,
-        minIMG:movies.smallThumbnail,
-        bigImg:movies.bigThumbnail,
-        link:`https://www.youtube.com/embed/${movies.youtubeId}`,
-        
-    }
- })
-
-
-
- console.log(AllMovies);
-
+const AllMovies = movies.map((movies) => {
+  return {
+    title: movies.title,
+    year: movies.year,
+    time: `${Math.floor(movies.runtime / 60)}h ${movies.runtime % 60}m`,
+    lang: movies.language,
+    category: movies.categories,
+    summary: movies.summary,
+    rating: movies.imdbRating,
+    ID: movies.imdbId,
+    minIMG: movies.smallThumbnail,
+    bigImg: movies.bigThumbnail,
+    link: `https://www.youtube.com/embed/${movies.youtubeId}`,
+  };
+});
 
 //  render all movies***************************************
-
-
 function renderAllMovies() {
+  AllMovies.forEach((el) => {
+    const card = createElement(
+      "div",
+      "card shadow",
+      `
+<img
+src="${el.minIMG}"
+alt="movies"
+class="card-img"
+/>
+<div class="card-body">
+<h4 class="card-title">${el.title}</h4>
+<ul class="list-unstyled">
+  <li><strong> Year:   ${el.year} </strong></li>
+  <li><strong> language: ${el.lang}</strong></li>
+  <li><strong> Rating: ${el.rating}</strong></li>
+  <li><strong> Category: ${el.category}</strong></li>
+  <li><strong> Runtime: ${el.time}</strong></li>
+  <li><strong> ID: ${el.ID}</strong></li>
+</ul>
 
-    AllMovies.forEach((el)=>{
-        const card=document.createElement('div');
-        card.classList.add('shadow' , 'card')
-        card.innerHTML=`
-
-
-        <img
-        src="${el.minIMG}"
-        alt="movies"
-        class="card-img"
-      />
-      <div class="card-body">
-        <h4 class="card-title">${el.title}</h4>
-        <ul class="list-unstyled">
-          <li><strong> Year:   ${el.year} </strong></li>
-          <li><strong> language: ${el.lang}</strong></li>
-          <li><strong> Rating: ${el.rating}</strong></li>
-          <li><strong> Category: ${el.category}</strong></li>
-          <li><strong> Runtime: ${el.time}</strong></li>
-          <li><strong> ID: ${el.ID}</strong></li>
-        </ul>
-
-        <div class="social d-flex">
-          <button class="btn btn-danger m-2">You-Tube</button>
-          <button class="btn btn-primary m-2">Read More</button>
-          <button class="btn btn-warning m-2">Add Bookmark</button>
-        </div>
-      </div>
-
-        `;
-
-
-        $('.wrapper').appendChild(card)
-
-    })
-    
+<div class="social d-flex">
+  <button class="btn btn-danger m-2">You-Tube</button>
+  <button class="btn btn-primary m-2">Read More</button>
+  <button class="btn btn-warning m-2">Add Bookmark</button>
+</div>
+</div>
+`
+    );
+    $(".wrapper").appendChild(card);
+  });
 }
-renderAllMovies()
+renderAllMovies();
+
+// search film********************************************************************
+
+const findFilm = (regexp , rating) => {
+  
+  return AllMovies.filter((film) => {
+    return film.title.match(regexp) && film.rating>=rating;
+  });
+};
+
+// // search film********************************************************************
+
+// // search film listener********************************************************************
+
+$("#submitForm").addEventListener("submit", () => {
+
+  $('.wrapper').innerHTML=`<span class="loader orta_load"></span>`;
+
+  const searchValue = $("#filmname").value;
+
+
+  const filmRating=$('#ratingname').value;
+
+
+
+  const regexp = new RegExp(searchValue, "gi");
+
+  const searchResult = findFilm(regexp, filmRating);
+  
+
+
+
+
+setTimeout(()=>{
+  if(searchResult.length>0){
+  
+    searchResultRunder(searchResult)
+ 
+    $('.card-res').classList.remove('d-none');
+ 
+    
+  
+    $('#res').innerHTML=`<strong >${searchResult.length}</strong> ta film topildi !`;
+
+    if(searchValue.length===0){
+
+      $('.card-res').classList.add('d-none');
+
+    }
+
+}else{
+
+      $('.card-res').classList.add('d-none');
+  $('.wrapper').innerHTML = '<p class="malumottop text-center">Bunday film topilmadi !</p>'
+}
+
+},2000)
+  // searchResultRunder(searchResult);
+});
+
+// // search film listener********************************************************************
+
+
+
+
+
+
+
+
+function searchResultRunder(data = []) {
+  $(".wrapper").innerHTML = "";
+  data.forEach((el) => {
+    const card = createElement(
+      "div",
+      "card shadow",
+      `
+<img
+src="${el.minIMG}"
+alt="movies"
+class="card-img"
+/>
+<div class="card-body">
+<h4 class="card-title">${el.title}</h4>
+<ul class="list-unstyled">
+  <li><strong> Year:   ${el.year} </strong></li>
+  <li><strong> language: ${el.lang}</strong></li>
+  <li><strong> Rating: ${el.rating}</strong></li>
+  <li><strong> Category: ${el.category}</strong></li>
+  <li><strong> Runtime: ${el.time}</strong></li>
+  <li><strong> ID: ${el.ID}</strong></li>
+</ul>
+
+<div class="social d-flex">
+  <button class="btn btn-danger m-2">You-Tube</button>
+  <button class="btn btn-primary m-2">Read More</button>
+  <button class="btn btn-warning m-2">Add Bookmark</button>
+</div>
+</div>
+`
+    );
+    $(".wrapper").appendChild(card);
+  });
+}
--- scripts/utils.js
+++ scripts/utils.js
@@ -4,4 +4,15 @@ const $$=(selector)=>document.querySelectorAll(selector);
 
 
 
+// dynamic crete element
 
+const createElement =function(tagName, className, content){
+  const newElement = document.createElement(tagName);
+if(className){
+  newElement.setAttribute('class',className);
+}
+if(content){
+  newElement.innerHTML = content;
+}
+  return newElement
+}
